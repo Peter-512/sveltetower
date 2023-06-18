02211d453a055aa391d464b9c53c3bbf410c6155
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-06-18 22:47:13 +0200

    removed commets

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,16 +3,4 @@ import { createClient } from "@supabase/supabase-js"
 export default createClient(
 	import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_URL,
 	import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY
-	// {
-	// 	realtime: {
-	// 		headers: {
-	// 			Authorization: `Bearer ${
-	// 				import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY
-	// 			}`,
-	// 		},
-	// 		params: {
-	// 			apiKey: import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY,
-	// 		},
-	// 	},
-	// }
 )

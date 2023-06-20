73027c331414b747550eaebd5272ba7f2fea39e0
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-06-20 15:38:26 +0200

    fixing migration

--- supabase/migrations/20230620125730_seed_test_users.sql
+++ supabase/migrations/20230620125730_seed_test_users.sql
@@ -1,3 +1,3 @@
-INSERT INTO USERS (id, email, name) 
-VALUES ('1', 'test@user.com', 'Test User'),
-       ('2', 'peter@test.com', 'Peter Parker');
+INSERT INTO public.USERS (email, name) 
+VALUES ('test@user.com', 'Test User'),
+       ('peter@test.com', 'Peter Parker');
